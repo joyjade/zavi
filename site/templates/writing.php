5c81acfb684711e85b7bc4611fcdc2df50c0473e
JJ <jadeandpearls@gmail.com>
2025-02-20 17:24:20 -0700

    writing sections;

--- site/templates/writing.php
+++ site/templates/writing.php
@@ -1,12 +1,22 @@
 <?php snippet('nav') ?>
 <div class="content">
   <ul class="flex writing">
+    <h3>Poetry</h3>
     <?php foreach(  $page-> poetry() -> toStructure() as $poetry ): ?> 
       <li>
         <p>“<a href="<?=$poetry -> link() ?>"><?=$poetry -> title()?></a>,” ⟶
-        <?=$poetry -> publisher() ?></p>
+        <?=$poetry -> publisher() ?> (<?=$poetry -> date() ?>)</p>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  <ul class="flex writing">
+    <h3>Essays</h3>
+    <?php foreach(  $page-> essays() -> toStructure() as $essay ): ?> 
+      <li>
+        <p>“<a href="<?=$essay -> link() ?>"><?=$essay -> title()?></a>,” ⟶
+        <?=$essay -> publisher() ?></p>
       </li>
     <?php endforeach ?>
-    </ul>
+  </ul>
 </div>
 <?php snippet('footer') ?>
