bf191ff4baecbd6fa9e7143c92c48ca267852797
JJ <jadeandpearls@gmail.com>
2025-02-23 15:42:10 -0700

    add a squiggly

--- site/snippets/link_item.php
+++ site/snippets/link_item.php
@@ -2,7 +2,7 @@
   <?php foreach(  $section -> toStructure() as $item ): ?> 
     <li>
       <a href="<?=$item -> link() ?>" target="_blank">
-        <p>“<span class="link_title"><?=$item -> title()?></span>,” ⟶
+        <p>“<span class="link_title"><?=$item -> title()?></span>,” ~
         <span class="publisher"><?=$item -> publisher() ?></span>
         (<?=$item -> date() ?>)</p>
       </a>
