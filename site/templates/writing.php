5623183c5990e57b06affbcd9a94e352b3aadc16
JJ <jadeandpearls@gmail.com>
2025-06-01 13:29:29 -0600

    update comma

--- site/templates/writing.php
+++ site/templates/writing.php
@@ -8,7 +8,7 @@
       <?php foreach(  $page->$genre() -> toStructure() as $item ): ?> 
         <li>
           <a href="<?=$item -> link() ?>" target="_blank">
-            <p>“<span class="link_title"><?=$item -> title()?></span>,” ~
+            <p>“<span class="link_title"><?=$item -> title()?></span>” ~
             <span class="publisher"><?=$item -> publisher() ?></span>
             (<?=$item -> date() ?>)</p>
           </a>
